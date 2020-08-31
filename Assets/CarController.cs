fc5fb5e4fcea515bc0ddc5af97ea28995134a19c
shingo2232-droid <shingo.kanda@gmail.com>
2020-08-31 19:43:14 +0900

    lesson6課題提出

---
+++ Assets/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    //カメラを入れる
+    private GameObject camera;
+    //カメラと車の距離
+    private float difference;
+    // Use this for initialization
+    void Start () {
+        this.camera = GameObject.Find("Main Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.difference = camera.transform.position.z - this.transform.position.z;
+        
+        if (this.transform.position.z < this.camera.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //カメラを入れる
+    private GameObject camera;
+    //カメラとコインの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.camera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.difference = camera.transform.position.z - this.transform.position.z;
+
+        if (this.transform.position.z < this.camera.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
